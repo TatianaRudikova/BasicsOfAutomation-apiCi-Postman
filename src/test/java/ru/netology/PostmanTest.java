bc3ff9602bfbe715bce26d5a97d3d5843e972ccf
Tatiana Rudikova <tatjana_tv@list.ru>
2023-01-17 15:23:23 +0300

    сломан тест, проверка ci

--- src/test/java/ru/netology/PostmanTest.java
+++ src/test/java/ru/netology/PostmanTest.java
@@ -11,7 +11,7 @@ public class PostmanTest {
         given()
                 .baseUri("https://postman-echo.com")
                 .contentType("text/plain; charset=UTF-8")
-                .body("API тест")
+                .body("API тест123")
                 .when()
                 .post("/post")
                 .then()
